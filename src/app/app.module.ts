e72da1a9dc67a3366bfdd4e24d357d56b3b5bb75
CYBERDOM2\ivsof1 <ivan.sofeski@cybercom.com>
2017-09-19 15:08:07 +0200

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MdButtonModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
