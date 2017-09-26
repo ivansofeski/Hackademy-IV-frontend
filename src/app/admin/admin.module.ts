de90014a18aa41a0cce75047204b79000196346e
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-09-26 15:34:47 +0200

    created data get/set service to be used in Admin epic

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 
+// Services
+import { DataService } from './services/data.service';
+
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminComponent]
+  declarations: [AdminComponent],
+  providers: [DataService]
 })
 export class AdminModule { }
