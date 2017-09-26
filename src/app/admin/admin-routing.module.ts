eb4d2800f5f9fdb7b13e087f88469faa45e10812
ticooo95 <ahmed1995bazzara@gmail.com>
2017-09-26 16:46:31 +0200

    updated on organization list

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { OrganizationListComponent } from './organization-list/organization-list.component';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent , children:[
-//    {path:'admin', AdminPanelComponent},
-//    {path:'organizations', OrganizatonsListComponent}
-//    {path:'organizations/new', OrganizatonFormComponent}
-//    {path:'organizations/view/:id', OrganizatonViewComponent}
+  {
+    path: '', component: AdminComponent, children: [
+      //    {path:'admin', AdminPanelComponent},
+      //    {path:'organizations', OrganizatonsListComponent}
+      //    {path:'organizations/new', OrganizatonFormComponent}
+      //    {path:'organizations/view/:id', OrganizatonViewComponent}
+      { path: 'organizations/', component: OrganizationListComponent }
 
-]},
+    ]
+  },
 
 ];
 
--- src/app/admin/organization-list/organization-list.component.ts
+++ src/app/admin/organization-list/organization-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-organization-list',
   templateUrl: './organization-list.component.html',
   styleUrls: ['./organization-list.component.scss']
 })
-export class OrganizationListComponent implements OnInit {
+export class OrganizationListComponent implements OnInit, OnDestroy {
+  
+  errorMessage: string;
+  fetchedData: any[];
+  organizationsFilePath: string = '../../assets/organizations.json';
 
-  constructor() { }
+  // Constructor here
+  constructor(private _dataService: DataService) { }
 
   ngOnInit() {
+    this._dataService.get(this.organizationsFilePath)
+      .subscribe(
+      res => {
+        this.fetchedData = res;
+        console.log(this.fetchedData);
+      },
+      error => this.errorMessage = <any>error);
+  };
+  
+  ngOnDestroy(): void {
+    
   }
 
 }
