2101fdd0e685f7f61bad5f02306e792a8b908aa4
Yaseen <mhd.awazh@gmail.com>
2017-09-26 15:49:56 +0200

    adding support for angular material

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     ProjectsRoutingModule
   ],
   declarations: [ProjectsComponent, ProjectListComponent]
