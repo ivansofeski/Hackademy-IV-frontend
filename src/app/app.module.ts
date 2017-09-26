9ccc30c4fe90303702836a818551fd05df7dd129
Al-Ayham Saleh <alayham@alayham.com>
2017-09-26 10:00:23 +0200

    Created the admin module and a base admin component, and linked it the the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    AdminModule,
     BrowserAnimationsModule
   ],
   providers: [],
