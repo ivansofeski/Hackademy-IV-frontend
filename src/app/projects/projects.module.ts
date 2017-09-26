a3a6b12a211f51464b851d38a2bf10220b212e67
Yaseen <mhd.awazh@gmail.com>
2017-09-26 11:09:28 +0200

    Project-list component created and routed

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProjectsRoutingModule
   ],
-  declarations: [ProjectsComponent]
+  declarations: [ProjectsComponent, ProjectListComponent]
 })
 export class ProjectsModule { }
