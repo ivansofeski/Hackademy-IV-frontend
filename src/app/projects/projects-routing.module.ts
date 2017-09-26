f02e48ea893113b3a0ed359c8ce62236d38f5970
Yaseen <mhd.awazh@gmail.com>
2017-09-26 11:09:28 +0200

    Project-list component created and routed

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProjectsComponent} from './projects.component';
+import {ProjectListComponent} from './project-list/project-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProjectsComponent , children:[
+    {path: '', component: ProjectListComponent},
+//    {path:'organizations', OrganizatonsListComponent}
+//    {path:'organizations/new', OrganizatonFormComponent}
+//    {path:'organizations/view/:id', OrganizatonViewComponent}
+
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
