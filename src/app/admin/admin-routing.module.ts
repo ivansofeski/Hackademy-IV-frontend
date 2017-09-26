848b556c91c1b09ded1e44997e6069188b5fa2f4
Al-Ayham Saleh <alayham@alayham.com>
2017-09-26 10:41:52 +0200

    initial routing and component names for admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AdminComponent , children:[
+//    {path:'admin', AdminPanelComponent},
+//    {path:'organizations', OrganizatonsListComponent}
+]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
