ee661d61b779202a2864f47a2398c611ad4eb073
MhdBarazi <mhd.barazi@gmail.com>
2017-09-27 16:18:45 +0200

    Showing the OrgList

--- src/app/admin/organization-list/organization-list.component.ts
+++ src/app/admin/organization-list/organization-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../services/data.service';
+import {DataSource} from '@angular/cdk/collections';
+import {MdPaginator} from '@angular/material';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-organization-list',
