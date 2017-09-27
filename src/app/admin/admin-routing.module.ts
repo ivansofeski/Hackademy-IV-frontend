cc7bebed693babe970ee542c3fae81285ed99ade
Samatar Mahamed <samatar.mahamed@gmail.com>
2017-09-27 11:00:25 +0200

    updated admin-routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,14 +5,6 @@ import { AdminComponent } from './admin.component';
 import { OrganizationListComponent } from './organization-list/organization-list.component';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent , children: [
-//    {path:'admin', AdminPanelComponent},
-//    {path:'organizations', OrganizatonsListComponent}
-    {path: 'admin/gitorganizations/new', component: OrganizationFormComponent}
-//    {path:'organizations/view/:id', OrganizatonViewComponent}
-
-]},
-
   { path: 'admin', component: AdminComponent },
   { path: 'admin/organizations', component: OrganizationListComponent },
   // { path: 'admin/organizations/new', OrganizatonFormComponent },
