01666319c058bc9ae08e2fb6b76fe8650eb658a5
Yaseen <mhd.awazh@gmail.com>
2017-09-27 23:28:34 +0200

    fixing conflict

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,40 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
 import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModule , MdProgressBarModule} from '@angular/material';
-=======
-import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModule , MatProgressBarModule} from '@angular/material';
->>>>>>> adding project description structure and the progress bar module
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule,
     MdButtonModule,
     MdMenuModule,
     MdToolbarModule,
     MdIconModule,
     MdCardModule,
-<<<<<<< HEAD
     MdProgressBarModule
-=======
-    MatProgressBarModule
->>>>>>> adding project description structure and the progress bar module
   ],
   exports: [
     CommonModule,
-    HttpClientModule,
     MdButtonModule,
     MdMenuModule,
     MdToolbarModule,
     MdIconModule,
     MdCardModule,
-<<<<<<< HEAD
     MdProgressBarModule
-=======
-    MatProgressBarModule
->>>>>>> adding project description structure and the progress bar module
   ],
   declarations: []
 })
