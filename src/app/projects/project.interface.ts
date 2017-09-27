bf4703f9653681058707890f69b536d6597cc2ab
Adnan Hasani <32267630+AdiHasani@users.noreply.github.com>
2017-09-27 11:28:56 +0200

    Changes in interface file and json file

---
+++ src/app/projects/project.interface.ts
@@ -0,0 +1,14 @@
+export interface Project {
+    id: number;
+    projectName: string;
+    toDate: string;
+    fromDate: string;
+    address: string;            
+    neededFunding: number;
+    raisedFunding: number;
+    description: string;
+    mainImage: string;
+    projectManager: string;
+    projectId: string;
+    organizationName: string
+}
