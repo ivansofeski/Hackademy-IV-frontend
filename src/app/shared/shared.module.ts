106d869c05b3b96334ddc5fd2d16cbebffc0a2c9
Yaseen <mhd.awazh@gmail.com>
2017-09-27 02:45:45 +0200

    adding project description structure and the progress bar module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModule , MdProgressBarModule} from '@angular/material';
+=======
+import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModule , MatProgressBarModule} from '@angular/material';
+>>>>>>> adding project description structure and the progress bar module
 
 @NgModule({
   imports: [
@@ -12,7 +16,11 @@ import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModu
     MdToolbarModule,
     MdIconModule,
     MdCardModule,
+<<<<<<< HEAD
     MdProgressBarModule
+=======
+    MatProgressBarModule
+>>>>>>> adding project description structure and the progress bar module
   ],
   exports: [
     CommonModule,
@@ -22,7 +30,11 @@ import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModu
     MdToolbarModule,
     MdIconModule,
     MdCardModule,
+<<<<<<< HEAD
     MdProgressBarModule
+=======
+    MatProgressBarModule
+>>>>>>> adding project description structure and the progress bar module
   ],
   declarations: []
 })
