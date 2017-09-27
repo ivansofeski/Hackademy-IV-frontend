91697694d8c870a5a36f85a94f23ffa3b40152f6
Ehsan <ehsan.msc87@gmail.com>
2017-09-27 16:14:07 +0200

    project service is fixed now!

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -4,15 +4,15 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/Observable/throw';
 import {Observable} from 'rxjs/Observable';
-import { IProject } from './projectInterface';
+import { Project } from './project.interface';
 
 @Injectable()
 export class ProjectService {
   _projectUrl='../assets/mockdata/projects.json';
   constructor(private _http: HttpClient){}
   
-  getProjects():Observable<IProject[]> {
-    return this._http.get<IProject[]>(this._projectUrl)
+  getProjects():Observable<Project[]> {
+    return this._http.get<Project[]>(this._projectUrl)
                 .do(data => console.log('info about projects is being retrieved---'+data.length))
                 .catch(this.handlerError);
   }
