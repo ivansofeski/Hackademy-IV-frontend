ad96c7127f0985fc621117901d6993ef61748473
Yaseen <mhd.awazh@gmail.com>
2017-09-27 22:37:10 +0200

    conflict resolved

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProjectsComponent} from './projects.component';
+import {ProjectListComponent} from './project-list/project-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProjectsComponent , children: [
+    {path: '', component: ProjectListComponent},
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
