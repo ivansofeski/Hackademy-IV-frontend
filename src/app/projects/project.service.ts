29b8d6023e1545fef3b435fa470e15dd23ecc199
Yaseen <mhd.awazh@gmail.com>
2017-09-28 15:32:37 +0200

    getting the project from the service and adding some styles

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient,  HttpErrorResponse } from '@angular/common/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
-import 'rxjs/add/observable/throw';
+import 'rxjs/add/Observable/throw';
 import {Observable} from 'rxjs/Observable';
 import { Project } from './project.interface';
 
 @Injectable()
 export class ProjectService {
-  _projectUrl='../assets/mockdata/projects.json';
+  _projectUrl = '../assets/mockdata/projects.json';
   constructor(private _http: HttpClient){}
 
-  getProjects():Observable<Project[]> {
+  getProjects(): Observable <Project[]> {
     return this._http.get<Project[]>(this._projectUrl)
-                .do(data => console.log('info about projects is being retrieved---'+data.length))
+                .do(data => console.log('info about projects is being retrieved---' + data.length))
                 .catch(this.handlerError);
   }
 
