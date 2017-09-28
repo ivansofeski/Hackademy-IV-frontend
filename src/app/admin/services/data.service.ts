0cddd86ca75cd58c53042b13c58f278b4b98499b
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-09-28 14:31:06 +0200

    Added more methods specific for querying Organizations e.g. organization({id:1})

--- src/app/admin/services/data.service.ts
+++ src/app/admin/services/data.service.ts
@@ -13,9 +13,20 @@ export class DataService {
     projects: 'projects.json'
   };
 
-  private _get(path: string): Observable<any> {
+  private _get(path: string, options?: Object): Observable<any> {
     if (path === undefined) {
       return;
+    }
+
+    if (options !== undefined && Object.keys(options).length > 0) {
+      // tslint:disable-next-line:prefer-const
+      for (let key in options) {
+        if (options.hasOwnProperty(key)) {
+          // tslint:disable-next-line:prefer-const
+          let _query = `${key}='${options[key]}'`;
+          path += _query;
+        }
+      }
     }
 
     return this.http.get(path)
@@ -35,10 +46,17 @@ export class DataService {
     return _inserted;
   }
 
+  // Subject to change get => loadData/getData/grab etc.
   // tslint:disable-next-line:member-ordering
   get = {
+    organization: (options: Object): any => {
+      return this._get(this.paths.root + this.paths.organizations, options);
+    },
     organizations: (): any => {
       return this._get(this.paths.root + this.paths.organizations);
+    },
+    project: (options: Object): any => {
+      return this._get(this.paths.root + this.paths.projects, options);
     },
     projects: (): any => {
       return this._get(this.paths.root + this.paths.projects);
