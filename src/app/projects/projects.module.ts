915ceb21064f97063335b80ab6cec72e021acdd2
neirasah <neira.sahbegovic@gmail.com>
2017-09-28 11:24:02 +0200

    removed the shared module as it is not needed anymore so that we can merge correctly

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
+
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 
@@ -8,7 +8,6 @@ import { ProjectsComponent } from './projects.component';
   imports: [
     CommonModule,
     ProjectsRoutingModule,
-    SharedModule
   ],
   declarations: [ProjectsComponent]
 })
