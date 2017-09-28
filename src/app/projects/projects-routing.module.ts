ee0847fa2acd61831ceef6b25179a81fd8bd975c
neirasah <neira.sahbegovic@gmail.com>
2017-09-28 12:06:15 +0200

    removed unneeded import

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProjectsComponent } from "./projects.component";
 
 
 const routes: Routes = [];
