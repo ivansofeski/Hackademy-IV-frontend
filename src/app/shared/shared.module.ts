38cd7c93a1042fcbcf1b1e00f2a6f6105925d5e1
Ehsan <ehsan.msc87@gmail.com>
2017-09-28 14:09:46 +0200

    navbar component been created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModule, MdProgressBarModule } from '@angular/material';
+import { NavbarComponent } from './navbar.component';
 
 
 @NgModule({
@@ -25,6 +26,6 @@ import { MdCardModule, MdIconModule, MdToolbarModule, MdMenuModule, MdButtonModu
     MdCardModule,
     MdProgressBarModule,
   ],
-  declarations: []
+  declarations: [NavbarComponent]
 })
 export class SharedModule { }
