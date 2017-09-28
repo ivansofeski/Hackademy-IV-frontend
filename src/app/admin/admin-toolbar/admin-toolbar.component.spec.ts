fcad15b5d2d1b149863a9e52d160e8bfce5c3af9
Al-Ayham Saleh <alayham@alayham.com>
2017-09-28 10:24:47 +0200

    Added a few unit tests

--- src/app/admin/admin-toolbar/admin-toolbar.component.spec.ts
+++ src/app/admin/admin-toolbar/admin-toolbar.component.spec.ts
@@ -1,12 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By }              from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AdminToolbarComponent } from './admin-toolbar.component';
 import { SharedModule } from '../../shared/shared.module';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AdminToolbarComponent', () => {
   let component: AdminToolbarComponent;
   let fixture: ComponentFixture<AdminToolbarComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,10 +23,32 @@ describe('AdminToolbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AdminToolbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should be created', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should render a material toolbar', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('md-toolbar.mat-toolbar'));
+    //el = de.nativeElement;
+    expect(de).toBeTruthy();
+  });
+
+  it('should render material raised buttons', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('button.mat-raised-button'));
+    //el = de.nativeElement;
+    expect(de).toBeTruthy();
+  });
+
+  it('should render buttons with material primary color', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('button.mat-primary'));
+    //el = de.nativeElement;
+    expect(de).toBeTruthy();
+  });
+
 });
