b857364dd2b0e8324bf293a75b51ff32411a8a09
Al-Ayham Saleh <alayham@alayham.com>
2017-10-02 14:16:30 +0200

    created the test data file for testing admin features

---
+++ src/app/admin/test-data.ts
@@ -0,0 +1,92 @@
+export const testData = {
+    orgList: [
+        {
+            "id": 1,
+            "orgId": "ORG12-ISP9000-71812",
+            "name": "Org. A",
+            "address": "Främsteby Karlsborg 3, 28236, Malmö",
+            "contact": {
+                "phone": "046-8807494",
+                "email": "support@orten.io",
+                "person": "Ivan"
+            },
+            "password": "@#aaOB23WEpp!",
+            "billing": "Handslagarevägen 28, 28199, Lund",
+            "description": "Hello compassionate hyena one up infectiously scowled octopus one far walrus goodness as gosh when inclusively lenient husky this bluebird underneath this where more jeepers under thoughtful."
+        }, 
+        {
+            "id": 2,
+            "orgId": "ORG15-ISP9000-56923",
+            "name": "Org. B",
+            "address": "Norrfjäll 58, 26215, Goteborg",
+            "contact": {
+                "phone": "016-4859091",
+                "email": "support@jayway.com",
+                "person": "Olaf"
+            },
+            "password": "qwerty@#45",
+            "billing": "Kläppinge 67, 29122, Stockholm",
+            "description": "Far more some goodness alas hello jeepers hello amongst jeepers seagull anathematically well drove more one bound through while much in however overshot strived orca in burned showed however by."
+        }
+    ],
+    projectList: [
+        {
+            "id": 1,
+            "projectName": "Childrens Rights",
+            "toDate": "2017/09/01",
+            "fromDate": "2017/12/01",
+            "address": "Ormvråksgatan 2, 21562, Malmö",
+            "neededFunding": 20000,
+            "raisedFunding": 2000,
+            "description": "Litora sed purus lorem vel quam sagittis euismod lacinia mattis nec, habitasse nunc lorem vivamus torquent suspendisse ac aenean leo consectetur taciti, donec sociosqu dictumst mollis vivamus nam aliquet nibh vestibulum magna mauris duis nulla fermentum enim convallis, ipsum urna curabitur netus et etiam, suspendisse ligula volutpat fames aenean.",
+            "mainImage": "./assets/photos/project001.jpg",
+            "projectManager": "Mats Nilsson",
+            "projectId": "001",
+            "organizationName": "Save the children"
+        },
+        {
+            "id": 2,
+            "projectName": "Climate changes in Sweden",
+            "toDate": "2017/09/15",
+            "fromDate": "2017/11/15",
+            "address": "Malmögatan 2, 26252, Göteborg",
+            "neededFunding": 40000,
+            "raisedFunding": 8000,
+            "description": "Litora sed purus lorem vel quam sagittis euismod lacinia mattis nec, habitasse nunc lorem vivamus torquent suspendisse ac aenean leo consectetur taciti, donec sociosqu dictumst mollis vivamus nam aliquet nibh vestibulum magna mauris duis nulla fermentum enim convallis, ipsum urna curabitur netus et etiam, suspendisse ligula volutpat fames aenean.",
+            "mainImage": "./assets/photos/project002.jpg",
+            "projectManager": "Johan Ericsson",
+            "projectId": "002",
+            "organizationName": "Unicef"
+    
+        },
+        {
+            "id": 3,
+            "projectName": "Childrens Rights",
+            "toDate": "2017/09/01",
+            "fromDate": "2017/12/01",
+            "address": "Ormvråksgatan 2, 21562, Malmö",
+            "neededFunding": 20000,
+            "raisedFunding": 2000,
+            "description": "Litora sed purus lorem vel quam sagittis euismod lacinia mattis nec, habitasse nunc lorem vivamus torquent suspendisse ac aenean leo consectetur taciti, donec sociosqu dictumst mollis vivamus nam aliquet nibh vestibulum magna mauris duis nulla fermentum enim convallis, ipsum urna curabitur netus et etiam, suspendisse ligula volutpat fames aenean.",
+            "mainImage": "./assets/photos/project003.jpg",
+            "projectManager": "Mats Nilsson",
+            "projectId": "003",
+            "organizationName": "Save the children"
+        },
+        {
+            "id": 4,
+            "projectName": "Climate changes in Sweden",
+            "toDate": "2017/09/15",
+            "fromDate": "2017/11/15",
+            "address": "Malmögatan 2, 26252, Göteborg",
+            "neededFunding": 40000,
+            "raisedFunding": 8000,
+            "description": "Litora sed purus lorem vel quam sagittis euismod lacinia mattis nec, habitasse nunc lorem vivamus torquent suspendisse ac aenean leo consectetur taciti, donec sociosqu dictumst mollis vivamus nam aliquet nibh vestibulum magna mauris duis nulla fermentum enim convallis, ipsum urna curabitur netus et etiam, suspendisse ligula volutpat fames aenean.",
+            "mainImage": "./assets/photos/project004.jpg",
+            "projectManager": "Johan Ericsson",
+            "projectId": "004",
+            "organizationName": "Unicef"
+    
+        }
+    ]
+}
