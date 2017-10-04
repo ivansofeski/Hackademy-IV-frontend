d1560729d341a19aa60f6cee7a412415a397adcb
MhdBarazi <mhd.barazi@gmail.com>
2017-10-04 16:39:39 +0200

    complete unit test for all fields on project list component

--- src/app/admin/project-list/project-list.component.spec.ts
+++ src/app/admin/project-list/project-list.component.spec.ts
@@ -11,29 +11,106 @@ import { By } from '@angular/platform-browser';
 describe('ProjectListComponent', () => {
   let component: ProjectListComponent;
   let fixture: ComponentFixture<ProjectListComponent>;
-  let de:      DebugElement;
-  let el:      HTMLElement;
+  let debug: DebugElement;
+  let debugEl: DebugElement[];
+  let HTMLelement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectListComponent ],
-      imports: [ SharedModule, RouterTestingModule ],
-      providers: [ DataService ]
+      declarations: [ProjectListComponent],
+      imports: [SharedModule, RouterTestingModule],
+      providers: [DataService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
     let dataService = fixture.debugElement.injector.get(DataService);
-    let spy = spyOn(dataService,'getProjects').and.returnValue(Observable.of(testData.projectList));
+    let spy = spyOn(dataService, 'getProjects').and.returnValue(Observable.of(testData.projectList));
+    fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
-});
 
+
+  it('should display all projects in rows', () => {
+    debugEl = fixture.debugElement.queryAll(By.css('table tbody tr'));
+    expect(debugEl.length).toBe(testData.projectList.length);
+  });
+
+  it('should display project names', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].projectName);
+  });
+
+  it('should NOT display project id', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].projectId);
+  });
+
+
+  it('should display project manager', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].projectManager);
+  });
+
+  it('should display project address', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].address);
+  });
+
+  it('should display project fromdate', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].fromDate);
+  });
+
+  it('should display project todate', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].toDate);
+  });
+
+
+  it('should display project image', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('img[src="' + testData.projectList[index].mainImage + '"]'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement).toBeTruthy();
+  });
+
+  it('should NOT display project description', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).not.toContain(testData.projectList[index].description);
+  });
+
+  it('should display project neededFunding', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].neededFunding.toString());
+  });
+  it('should display Fundings raisedFunding', () => {
+    let index: number = Math.floor(testData.projectList.length * Math.random());
+    debug = fixture.debugElement.query(By.css('tbody'));
+    HTMLelement = debug.nativeElement;
+    expect(HTMLelement.textContent).toContain(testData.projectList[index].raisedFunding.toString());
+  });
+});
 
