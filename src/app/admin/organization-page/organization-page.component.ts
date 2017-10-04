dccd62b22566d57c4a481f58a46b7321a60ba08d
Al-Ayham Saleh <alayham@alayham.com>
2017-10-04 15:58:51 +0200

    Removed a depricated import

--- src/app/admin/organization-page/organization-page.component.ts
+++ src/app/admin/organization-page/organization-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
-import { IOrganization, Organization } from '../interface/organization';
+import { Organization } from '../interface/organization';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
