6df1d8a1a155ee7c535cdc15295e0bb35f8b33d0
Al-Ayham Saleh <alayham@alayham.com>
2017-10-04 09:28:50 +0200

    Updating admin routing to show the project page component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,14 +5,18 @@ import { AdminComponent } from './admin.component';
 import { OrganizationListComponent } from './organization-list/organization-list.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { OrganizationPageComponent } from './organization-page/organization-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent, children: [
     { path: 'organizations', component: OrganizationListComponent },
-    { path: 'projects', component: ProjectListComponent },
     { path: 'organizations/new', component: OrganizationFormComponent },
     { path: 'organizations/view/:id', component: OrganizationPageComponent }
-  ]},
+
+    { path: 'projects', component: ProjectListComponent },
+//    { path: 'projects/new', component: ProjectFormComponent },
+    { path: 'projects/view/:id', component: ProjectPageComponent },
+]},
 ];
 
 @NgModule({
