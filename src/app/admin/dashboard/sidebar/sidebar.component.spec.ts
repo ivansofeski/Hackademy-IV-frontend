1bf2e02a943cacee696c7426ccd58412e2588cda
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-10-09 16:57:44 +0200

    Updated related test files to reflect changes for Admin Sidebar

--- src/app/admin/dashboard/sidebar/sidebar.component.spec.ts
+++ src/app/admin/dashboard/sidebar/sidebar.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { SharedModule } from '../../../shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
+
+// Components
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [ SidebarComponent ],
+      imports: [ SharedModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
@@ -20,5 +29,26 @@ describe('SidebarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a sidebar container', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('div.sidebar-container'));
+    el = de.nativeElement;
+    expect(de).toBeTruthy();
+  });
+
+  it('should render menu item(s)', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('li.item'));
+    el = de.nativeElement;
+    expect(de).toBeTruthy();
+  });
+
+  it('should render proper nested menu items', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('li.item.hasItems'));
+    //el = de.nativeElement;
+    expect(de).toBeTruthy();
   });
 });
