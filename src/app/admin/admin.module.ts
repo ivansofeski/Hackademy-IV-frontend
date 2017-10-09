7d19c06be05838091c9e24be71fd9d33c54af397
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-10-09 13:14:00 +0200

    Added links and organized them in the sidebar

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,12 +8,12 @@ import { OrganizationListComponent } from './organization-list/organization-list
 import { ProjectListComponent } from './project-list/project-list.component';
 import { AdminToolbarComponent } from './admin-toolbar/admin-toolbar.component';
 import { OrganizationPageComponent } from './organization-page/organization-page.component';
-
-// Services
-import { DataService } from './services/data.service';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
 import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+
+// Services
+import { DataService } from './services/data.service';
 
 @NgModule({
   imports: [
