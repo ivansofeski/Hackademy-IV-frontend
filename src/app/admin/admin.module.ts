79110a0602307be402c7b98781ce92aa540a4739
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-10-09 16:23:28 +0200

    Fixed routing to default admin page, finished sidebar with links menu in Admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,15 +1,15 @@
-import { OrganizationFormComponent } from './organization-form/organization-form.component';
 import { NgModule } from '@angular/core';
-
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
-import { OrganizationListComponent } from './organization-list/organization-list.component';
-import { ProjectListComponent } from './project-list/project-list.component';
+import { AdminRoutingModule } from './admin-routing.module';
+
+import { AdminComponent } from './admin.component';
 import { AdminToolbarComponent } from './admin-toolbar/admin-toolbar.component';
 import { OrganizationPageComponent } from './organization-page/organization-page.component';
+import { OrganizationFormComponent } from './organization-form/organization-form.component';
+import { OrganizationListComponent } from './organization-list/organization-list.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 
 // Services
