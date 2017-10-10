5e372b25f07e3161c6a14f7d19a85678e94383ea
neirasah <neira.sahbegovic@gmail.com>
2017-10-10 15:52:38 +0200

    Created component project-page for project overview

--- src/app/projects/project-page/project-page.component.ts
+++ src/app/projects/project-page/project-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectService } from '../project.service';
+import { Project } from '../project.interface';
 
 @Component({
   selector: 'app-project-page',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectPageComponent implements OnInit {
 
-  constructor() { }
+  _projectId: number = 0;
+  _project: any;
+  errors: any[] = [];
 
-  ngOnInit() {
+  donateOption1= 10;
+  donateOption2= 25;
+  donateOption3= 50;
+
+  constructor(
+    public route: ActivatedRoute, 
+    public router: Router,
+    private projectService: ProjectService) {  
+    }
+
+    get projectId():number{
+      return this._projectId;
+    }
+
+    set projectId(value:number){
+      this._projectId = value;
+      if(value > 0){
+        this.projectService.getProjects().subscribe(
+          res => {
+            this._project = res.filter((v, k) => v.id == value)[0];
+          },
+          error => {
+            console.log(error);
+            this.errors.push(error);
+          }
+        );
+      }
+    }
+
+    get project(){
+      return this._project;
+    }
+  
+  
+    ngOnInit() {
+      this.projectService.getProjects().subscribe(
+        res => {
+          console.log(res);
+          this._project = res;
+        },
+        error => this.errors.push(error)
+      ); 
+    
+
+      this.projectId = +this.route.snapshot.paramMap.get('id');
+    }
+  
+
+
+  onBack(): void {
+    this.router.navigate(['home']);
   }
 
 }
