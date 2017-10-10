c0f8c7515ea13f47ee050b934c67b970efbe0b00
neirasah <neira.sahbegovic@gmail.com>
2017-10-10 15:52:38 +0200

    Created component project-page for project overview

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {ProjectsComponent} from './projects.component';
 import {ProjectListComponent} from './project-list/project-list.component';
 import { GeolocationComponent } from '../geolocation/geolocation.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 const routes: Routes = [
   { path: '', component: ProjectsComponent , children: [
     {path: 'home', component: ProjectListComponent},
+    {path: 'projects/:id', component: ProjectPageComponent},
     { path: 'geo', component: GeolocationComponent}
   ]}
 ];
