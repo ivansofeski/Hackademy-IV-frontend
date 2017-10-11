78e6be780bc9336845a358eda40b4995fc8d3022
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-10-11 11:18:58 +0200

    Reconfigured code and wrote description of each snippet in it

--- src/app/admin/sidebar/sidebar.component.ts
+++ src/app/admin/sidebar/sidebar.component.ts
@@ -14,19 +14,49 @@ export class SidebarComponent implements OnInit {
   links = LINKS;
   lng = 'US';
   listItemEventName = 'touchend';
+  functions: Functions = new Functions();
+  mediaChanged = this.functions.mediaChanged;
+  toggleSidebar = this.functions.toggleSidebar;
+  toggle = this.functions.toggle;
 
-  mediaChanged(mq) {
+  constructor() { }
+
+  ngOnInit() {
+    if (matchMedia) {
+      const mq = matchMedia('(max-width: 1366px)');
+
+      mq['eventName'] = this.listItemEventName;
+      mq.addListener(this.mediaChanged);
+      this.mediaChanged(mq);
+    }
+  }
+
+}
+
+// Exported all custom-tailored functions into a class named "Functions"
+// So that is better readable, easier to access and to group up
+// Provides less Component Class polluting (instead of having all functions in the same class we instatiate them from another).
+export class Functions {
+  // Function that detects window.matchMedia changes and evaluates them to true or false
+  // We want to add a JavaScript event listener to each list item on the sidebar.
+  // To do that, we check the required media query if it's the right one.
+  // If YES, we add an event listener with paramaterized event name defined as class property => "listItemEventName"
+  // If NO, we remove the same event listener that we provided earlier.
+  mediaChanged(media) {
     const allItems = Array.from(document.querySelectorAll('li.item'));
+    const eventName = media.eventName ? media.eventName : media.currentTarget.eventName;
 
     if (allItems !== undefined && allItems.length > 0) {
       for (const item of allItems) {
-        mq.matches ?
-          item.addEventListener(this.listItemEventName, this.toggle) :
-          item.removeEventListener(this.listItemEventName, this.toggle);
+        media.matches ?
+          item.addEventListener(eventName, this.toggle) :
+          item.removeEventListener(eventName, this.toggle);
       }
     }
   }
 
+  // Function that toggles the sidebar slide-in/slide-out visual effect with an overlay on the main-panel.
+  // To do that, we add another class name to the whole wrapper and then set CSS rules to it's corresponding css file.
   toggleSidebar(toggler: HTMLElement): void {
     if (toggler === undefined) {
       return;
@@ -41,11 +71,15 @@ export class SidebarComponent implements OnInit {
     }
   }
 
+  // Function that toggles CSS rules to the list items on the sidebar.
+  // Accepts only the triggering/firing HTML Element ("li") and depending on it's classList evaluates different result(s).
   toggle(evt: Event): EventListener {
     if (evt === undefined) {
       return;
     }
 
+    // toggler: Since TypeScript doesn't recognize "currentTarget" as a HTML Element from the evt (type of Event)
+    // we then must cast it (strong-type) to HTMLElement in order to have access to it's known properties.
     const toggler = <HTMLElement>evt.currentTarget;
     const container = toggler.parentElement;
     let lastSelectedItem;
@@ -53,12 +87,16 @@ export class SidebarComponent implements OnInit {
 
     switch (true) {
       case toggler.classList.contains('hasItems'):
+        // This is where we set values for "ul.item-list > li.item.hasItems" (nested item),
+        // so that we evaluate further down.
         classes = {
           container: 'item-list',
           query: '.item.expanded',
           toToggle: 'expanded'
         };
         break;
+        // This is where we set values for "ul.item-list.sub > li.item" (sub-item),
+        // so that we evaluate further down.
       case !toggler.classList.contains('hasItems'):
         classes = {
           container: 'sub',
@@ -70,6 +108,9 @@ export class SidebarComponent implements OnInit {
         break;
     }
 
+    // We want to set "classes" as an Object item which has exactly 3 properties
+    // in order to distinguish nested item vs. normal/sub item
+    // so that we can apply different CSS rules or do other stuff later on.
     if (classes !== undefined && Object.keys(classes).length === 3) {
       if (container !== undefined && container.classList.contains(classes.container)) {
         lastSelectedItem = container.querySelectorAll(classes.query)[0];
@@ -90,18 +131,10 @@ export class SidebarComponent implements OnInit {
       }
     }
 
+    // Since there are nested elements of "li" and both parent and child trigger this function,
+    // we have to stop propagation/delegation to not trigger both at the same time.
+    // To do that we use/implement "stopPropagation", "preventDefault" pre-defined methods of JavaScript events.
+    evt.preventDefault();
     evt.stopPropagation();
   }
-
-  constructor() { }
-
-  ngOnInit() {
-
-    if (matchMedia) {
-      const mq = matchMedia('(max-width: 1366px)');
-      mq.addListener(this.mediaChanged);
-      this.mediaChanged(mq);
-    }
-  }
-
 }
