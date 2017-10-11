756747bcbbc9398b885f1a7467dbb6a40d578e9d
Yaseen <mhd.awazh@gmail.com>
2017-10-11 13:22:07 +0200

    Testing with the local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  public currentUser: string = localStorage.getItem('currentUser');
+
+  constructor () {
+ // this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  ngOnInit(): void {
+
+    if (this.currentUser == null || this.currentUser === undefined) {
+      this.currentUser = JSON.stringify({
+        'id': 1,
+        'userName': 'Natasa',
+        'userSlogan': 'Do your home work in time',
+        'userImage': './assets/photos/userImage1.jpeg',
+        'wallet': { 'total': 0, 'donated': 125 },
+        'savedProject': [1, 2, 3, 4]
+      });
+      console.log(this.currentUser);
+      localStorage.setItem('currentUser', this.currentUser);
+    } else { console.log('asdasdasdas');}
+  }
+
 }
