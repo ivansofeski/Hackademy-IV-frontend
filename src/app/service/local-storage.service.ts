05aac4ac3ded9f61a0681cc06463efdf068a5ea5
Yaseen <mhd.awazh@gmail.com>
2017-10-12 17:11:26 +0200

    Adding the check and create for the user in the local storage from the app component

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class LocalStorageService {
+  user: string = localStorage.getItem('currentUser');
+  currentUser = {};
+  errors: any[] = [];
 
-  constructor() { }
+
+  constructor() {
+  }
+
+
+  create_localStorage_user() {
+    if (this.user == null || this.user === undefined) {
+
+      this.currentUser['id'] = Math.floor(Math.random() * 100000) + 1;
+      this.currentUser['userImage'] = './assets/photos/userImage1.jpeg';
+      console.log(this.currentUser);
+
+      this.user = JSON.stringify(this.currentUser);
+      console.log(this.user);
+      localStorage.setItem('currentUser', this.user);
+    }
+  }
+  getCurrentUser() {
+    if (this.user !== null || this.user !== undefined) {
+      console.log(JSON.parse(this.user));
+      return JSON.parse(this.user);
+    }
+  }
 
 }
