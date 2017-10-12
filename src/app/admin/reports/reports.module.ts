d283f0130cce6d1a3b656a69ed65decc72853e26
ticooo95 <ahmed1995bazzara@gmail.com>
2017-10-12 18:43:38 +0200

    Added Angular Material tables to the Closed Projects component and fixed some styling

--- src/app/admin/reports/reports.module.ts
+++ src/app/admin/reports/reports.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClosedProjectsComponent } from './closed-projects/closed-projects.component';
+import { MatTableModule, MatSortModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatSortModule,
+    MatCardModule
   ],
   declarations: [ClosedProjectsComponent]
 })
