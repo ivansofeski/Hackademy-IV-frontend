4333e24d0c74d7a2bbfcd818f9632921ae8e3ad6
Yaseen <mhd.awazh@gmail.com>
2017-10-15 13:23:35 +0200

    Adding getCurrentUser() and updateUser() and fixing the test for app component and adding save project on clock event

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class LocalStorageService {
-  user: string = localStorage.getItem('currentUser');
+  localStorageKey = 'currentUser';
+  user: string;
   currentUser = {};
   errors: any[] = [];
 
@@ -11,23 +12,49 @@ export class LocalStorageService {
   }
 
 
-  create_localStorage_user() {
+  createLocalStorageUser() {
+    this.user = localStorage.getItem(this.localStorageKey);
+
     if (this.user == null || this.user === undefined) {
 
       this.currentUser['id'] = Math.floor(Math.random() * 100000) + 1;
       this.currentUser['userImage'] = './assets/photos/userImage1.jpeg';
+      console.log('creating local storage user');
       console.log(this.currentUser);
 
       this.user = JSON.stringify(this.currentUser);
+      console.log('current user stringfied');
       console.log(this.user);
-      localStorage.setItem('currentUser', this.user);
-    }
-  }
-  getCurrentUser() {
-    if (this.user !== null || this.user !== undefined) {
-      console.log(JSON.parse(this.user));
-      return JSON.parse(this.user);
+      localStorage.setItem(this.localStorageKey, this.user);
     }
   }
 
+  getCurrentUser() {
+    this.user = localStorage.getItem(this.localStorageKey);
+    let userObject: any;
+    console.log('getcuurentUser()')
+    if (this.user == null || this.user === undefined){
+      console.log('user undefined --- create one')
+      this.createLocalStorageUser();
+      this.user = localStorage.getItem(this.localStorageKey);
+      userObject = JSON.parse(this.user);
+    } else if (this.user !== null || this.user !== undefined) {
+      console.log('get current user');
+      userObject = JSON.parse(this.user);
+    }
+    return userObject;
+  }
+
+  updateCurrnetUser(user: any): void {
+    this.user = localStorage.getItem(this.localStorageKey);
+
+    if (user !== null || user !== undefined) {
+      if (this.user !== null || this.user !== undefined) {
+        const updatedUser = JSON.stringify(user);
+        localStorage.setItem(this.localStorageKey, updatedUser);
+      } else {
+        this.createLocalStorageUser();
+      }
+    }
+  }
 }
