e5a5dcf56f7dd73d88e600350655ccb97f5e797a
Al-Ayham Saleh <alayham@alayham.com>
2017-10-16 13:22:06 +0200

    Added the shared module to the reports module

--- src/app/admin/reports/reports.module.ts
+++ src/app/admin/reports/reports.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClosedProjectsComponent } from './closed-projects/closed-projects.component';
-import { MatTableModule, MatSortModule, MatCardModule } from '@angular/material';
+import { MatTableModule, MatSortModule, MatCardModule, MatDatepickerModule } from '@angular/material';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     MatTableModule,
     MatSortModule,
-    MatCardModule
+    MatCardModule,
+    MatDatepickerModule
   ],
   declarations: [ClosedProjectsComponent]
 })
