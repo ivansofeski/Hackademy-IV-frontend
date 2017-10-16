febdeb9ce443d275d15b59b582812f4cd0a016e4
Al-Ayham Saleh <alayham@alayham.com>
2017-10-16 13:45:36 +0200

    removed unused imports

--- src/app/admin/reports/reports.module.ts
+++ src/app/admin/reports/reports.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClosedProjectsComponent } from './closed-projects/closed-projects.component';
-import { MatTableModule, MatSortModule, MatCardModule, MatDatepickerModule } from '@angular/material';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
