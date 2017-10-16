2a2308514ace9a72ecf30a34cf5407b5b7ec5bb1
Al-Ayham Saleh <alayham@alayham.com>
2017-10-16 13:44:50 +0200

    removed redundant imports

--- src/app/admin/reports/reports.module.ts
+++ src/app/admin/reports/reports.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClosedProjectsComponent } from './closed-projects/closed-projects.component';
 import { MatTableModule, MatSortModule, MatCardModule } from '@angular/material';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatTableModule,
-    MatSortModule,
-    MatCardModule
+    SharedModule,
   ],
   declarations: [ClosedProjectsComponent]
 })
