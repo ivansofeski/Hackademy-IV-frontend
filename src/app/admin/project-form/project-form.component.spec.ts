77c968961f5dddff0448cbe0c941725cd2aeddef
Al-Ayham Saleh <alayham@alayham.com>
2017-10-16 11:25:45 +0200

    wrote two tests for the name form field.

--- src/app/admin/project-form/project-form.component.spec.ts
+++ src/app/admin/project-form/project-form.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { By } from '@angular/platform-browser';
 
 import { ProjectFormComponent } from './project-form.component';
 import { SharedModule } from '../../shared/shared.module';
@@ -6,21 +9,19 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DataService } from '../services/data.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { testData } from '../test-data';
 
 describe('ProjectFormComponent', () => {
   let component: ProjectFormComponent;
   let fixture: ComponentFixture<ProjectFormComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+  let button:  HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ProjectFormComponent ],
-      imports: [
-        SharedModule,
-        FormsModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        RouterTestingModule
-      ],
+      imports: [ SharedModule ],
       providers: [ DataService ]
     })
     .compileComponents();
@@ -29,10 +30,30 @@ describe('ProjectFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService,'getOrganizations').and.returnValue(Observable.of(testData.orgList));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not accept an empty project name',() => {
+    component.projectControls.name.setValue(' ');
+    fixture.detectChanges();
+    expect(component.projectControls.name.valid).toBe(false);
+  });
+
+  it('should show an error message when an empty project name is supplied',() => {
+    component.projectControls.name.setValue(' ');
+    fixture.detectChanges();
+    button = fixture.debugElement.query(By.css('button.mat-primary')).nativeElement;
+    button.click();
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('mat-form-field.project-name mat-error'));
+    el = de.nativeElement;
+    expect(el.textContent).toContain('required');
+  });
+
+
 });
