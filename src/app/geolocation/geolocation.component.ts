05a981784233747ef0368273df3d24ef176c109b
Ehsan <ehsan.msc87@gmail.com>
2017-10-18 01:13:49 +0200

    dragable, clickable and searchbar are added now

--- src/app/geolocation/geolocation.component.ts
+++ src/app/geolocation/geolocation.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { ProjectService } from '../projects/project.service';
+import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
+import { Observable } from 'rxjs/Observable';
+//import { google } from '@agm/core/services/google-maps-types';
+declare var google: any;
 
 @Component({
   //selector: 'app-geolocation',
@@ -12,6 +17,16 @@ export class GeolocationComponent implements OnInit {
   zoom:number;
   radius:number;
   projects=[]
+  position;
+  geocoder;
+  inputAddressElm;
+  
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  @ViewChild("searchButton")
+  public searchButtonElementRef: ElementRef;
+
   iconUrl={url:"../assets/icons/pin.png",
             scaledSize: {
               height: 50,
@@ -24,17 +39,71 @@ export class GeolocationComponent implements OnInit {
             width: 25
           }
         }
-  constructor(private _projectService: ProjectService) { 
+  constructor(private _projectService: ProjectService,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) { 
     this.showPosition();
+    
   }
 
   ngOnInit() {
+    this.inputAddressElm= this.searchElementRef.nativeElement;
+    let searchButtElm = document.getElementById("searchButton");
+    this.mapsAPILoader.load().then(() => {
+      // let geocoder = new google.maps.Geocoder();
+      // searchButtElm.addEventListener("click", ()=>{
+      //   this.ngZone.run(()=>{
+      //     // this.search(inputAddressElm);
+      //     console.log("an address has been searched");
+      //     // let geocoder = new google.maps.Geocoder();
+      //     console.log(this.inputAddressElm.value);
+      //     geocoder.geocode( { 'address': this.inputAddressElm.value}, (results, status) =>{ 
+      //     // this.geoCodeResponse(results, status);
+      //     if (status !== 'OK') {
+      //       return;
+      //     }
+      //     let newCoords = results[0].geometry.location;
+      //     console.log(this.lat);
+      //     this.lat=newCoords.lat();
+      //     this.lat=newCoords.lat();
+      //     console.log(this.lat);
+      //     console.log(this.lng);
+      //     this.lng=newCoords.lng();
+      //     this.lng=newCoords.lng();
+      //     console.log(this.lng);
+      //     })
+      //   });
+      // });
+      
+      let autocomplete = new google.maps.places.Autocomplete(this.inputAddressElm, {
+        types: ["address"]
+      });
+
+      // this.searchButtonElementRef.nativeElement.addListener("click", this.search());
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });     
+    });
   }
 
   showPosition(){
     this.radius=4000;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position=>{this.lat = position.coords.latitude;
+        this.position=position;
         this.lng = position.coords.longitude;
         this.zoom = 12;
         console.log("Latitude: " + this.lat +
@@ -63,5 +132,67 @@ export class GeolocationComponent implements OnInit {
       this.lat = event.coords.lat;
       this.lng = event.coords.lng;
   }
+  // onClick(callback){
+  //     console.log("an address has been searched");
+  //     // let geocoder = new google.maps.Geocoder();
+  //     console.log(this.inputAddressElm.value);
+  //     let geocoder = new google.maps.Geocoder();
+  //     geocoder.geocode( { 'address': this.inputAddressElm.value}, (results, status) =>{ 
+  //     // this.geoCodeResponse(results, status);
+  //     if (status !== 'OK') {
+  //       return;
+  //     }
+  //     let newCoords = results[0].geometry.location;
+  //     console.log(this.lat);
+  //     this.lat=newCoords.lat();
+  //     console.log(this.lat);
+      
+  //     console.log(this.lng);
+  //     this.lng=newCoords.lng();
+  //     console.log(this.lng);
   
+  //     });  
+
+  // }
+  onClick(){
+    console.log("an address has been searched");
+    // let geocoder = new google.maps.Geocoder();
+    console.log(this.inputAddressElm.value);
+    this.getLatLan(this.inputAddressElm.value).subscribe(
+      result => {
+        // needs to run inside zone to update the map
+        this.ngZone.run(() => {
+          console.log(this.lat);
+            this.lat = result.lat();
+            console.log(this.lat);
+            console.log(this.lng);
+            this.lng = result.lng();
+            console.log(this.lng);
+        });
+    },
+    error => console.log(error),
+    () => console.log('Geocoding completed!')
+    );
+}
+
+getLatLan(address: string) {
+    console.log('Getting Address - ', address);
+    let geocoder = new google.maps.Geocoder();
+    return Observable.create(observer => {
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                observer.next(results[0].geometry.location);
+                observer.complete();
+            } else {
+                console.log('Error - ', results, ' & Status - ', status);
+                observer.next({});
+                observer.complete();
+            }
+        });
+    })
+}
+
+
+
+
 }
