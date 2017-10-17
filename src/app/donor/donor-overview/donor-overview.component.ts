3e99e5ce9d9b66e55aaec6c35c653944f13a5cd6
Yaseen <mhd.awazh@gmail.com>
2017-10-17 11:57:16 +0200

    pulling the saved projects in the donor overview page from the local storage saved project

--- src/app/donor/donor-overview/donor-overview.component.ts
+++ src/app/donor/donor-overview/donor-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {DonorOverviewService} from './donor-overview.service';
 import {Donor} from '../donor.interface';
 import {ProjectService} from '../../projects/project.service';
+import {LocalStorageService} from '../../service/local-storage.service';
 
 @Component({
   selector: 'app-donor-overview',
@@ -9,14 +10,19 @@ import {ProjectService} from '../../projects/project.service';
   styleUrls: ['./donor-overview.component.scss']
 })
 export class DonorOverviewComponent implements OnInit {
+  currentUser: any = {};
   errors: any[] = [];
   donorList: any[] = [];
   donor: Donor;
   projectList: any[] = [];
 
-  constructor(private _donorList: DonorOverviewService, private _projectService: ProjectService) { }
+  constructor(private _donorList: DonorOverviewService,
+              private _localStorageService: LocalStorageService,
+              private _projectService: ProjectService) { }
 
   ngOnInit() {
+    this.currentUser = this._localStorageService.getCurrentUser();
+
     this._donorList.getDonors().subscribe(
       res => {
         console.log(res);
@@ -27,12 +33,11 @@ export class DonorOverviewComponent implements OnInit {
       error => this.errors.push(error)
     );
 
-    this._projectService.getProjects().subscribe(
+    this._projectService.getTargettedProjects(this.currentUser.savedProject).subscribe(
       res => {
         console.log(res);
         this.projectList = res;
-      },
-      error => this.errors.push(error)
+      }
     );
   }
 
