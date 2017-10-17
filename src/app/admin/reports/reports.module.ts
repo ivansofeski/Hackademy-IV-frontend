094b7d174ac61a6ad95470f069181758e9cfdff4
ticooo95 <ahmed1995bazzara@gmail.com>
2017-10-17 15:03:53 +0200

    filter added to closed projects with old interfaces

--- src/app/admin/reports/reports.module.ts
+++ src/app/admin/reports/reports.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClosedProjectsComponent } from './closed-projects/closed-projects.component';
-import { MatTableModule, MatSortModule, MatCardModule } from '@angular/material';
+import { MatTableModule, MatSortModule, MatCardModule, MatSelectModule, MatDatepickerModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatTableModule,
     MatSortModule,
-    MatCardModule
+    MatCardModule,
+    MatDatepickerModule,
+    MatSelectModule
   ],
   declarations: [ClosedProjectsComponent]
 })
