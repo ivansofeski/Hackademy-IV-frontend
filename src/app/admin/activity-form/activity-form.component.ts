2ae238f96f6007ba704342073b79fa6c5cc4e2e3
Al-Ayham Saleh <alayham@alayham.com>
2017-10-20 03:33:47 +0200

    Added material theme color to the activity button

--- src/app/admin/activity-form/activity-form.component.ts
+++ src/app/admin/activity-form/activity-form.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormBuilder, FormGroup, Validator, Validators, AbstractCon
 import { Component, OnInit, ElementRef, ViewChild, DoCheck, QueryList } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { NanoValidators } from '../services/nano-validators';
+import { Project } from '../interface/project';
 
 @Component({
   selector: 'app-activity-form',
@@ -15,11 +16,10 @@ export class ActivityFormComponent implements OnInit {
 
   inputs: Function;
   errorsTwo: QueryList<String>;
-  loadedProject: Object;
+  loadedProject: Project;
 
   errors: any[] = [];
   activityControls = {
-
     descImage:   new FormControl('', []),
     name:        new FormControl('', [NanoValidators.required]),
     activityDate: new FormControl('', [Validators.required]),
@@ -28,12 +28,10 @@ export class ActivityFormComponent implements OnInit {
 
   setImagePath(elm: HTMLInputElement): void {
     if (elm === undefined || elm.value === '') {
-      console.log('elm', elm);
       return;
     }
 
     const _imgSelector = elm.parentElement.querySelectorAll('img')[0];
-    console.log('image selector', _imgSelector);
 
     if (_imgSelector !== undefined) {
       const fReader = new FileReader();
@@ -42,7 +40,6 @@ export class ActivityFormComponent implements OnInit {
       fReader.onloadend = function (event) {
         _imgSelector.src = event.target['result'];
         _imgSelector.parentElement.classList.add('no-border');
-        console.log();
       };
     }
   }
@@ -55,7 +52,7 @@ export class ActivityFormComponent implements OnInit {
       projects => {
         if (projects !== undefined && projects.length > 0) {
           this.loadedProject = projects.filter((v, k) => {
-            return v.id = projId;
+            return v.id == projId;
           })[0];
         }
       }
