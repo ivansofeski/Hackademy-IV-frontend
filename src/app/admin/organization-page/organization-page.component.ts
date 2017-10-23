b0897bbd00ec28c797773e5a80b3bee33aa239ce
Al-Ayham Saleh <alayham@alayham.com>
2017-10-23 12:45:16 +0200

    Updated the organization page component to use the new DataService

--- src/app/admin/organization-page/organization-page.component.ts
+++ src/app/admin/organization-page/organization-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../services/data.service';
+import { DataService } from '../../shared/services/data.service';
 import { Organization } from '../interface/organization';
 import { ActivatedRoute, Router } from '@angular/router';
 
