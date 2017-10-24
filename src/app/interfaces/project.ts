3f154b2e9606d94111ccf69a72af1a2959ee4d81
Al-Ayham Saleh <alayham@alayham.com>
2017-10-24 10:12:05 +0200

    consolidating with the backend project interface

--- src/app/interfaces/project.ts
+++ src/app/interfaces/project.ts
@@ -2,22 +2,28 @@ export interface Project {
     id: number;
     projectId: string;
     projectName: string;
-    projectManager: string;
     fromDate: string;
     toDate: string;
+    longitude?: number;
+    latitude?: number;
+    amountToBeRaised?: number;
+    raisedFunding: number;
+    description: string;
+    imageOrvideo?:string;
+    projectManager: string;    
+    recurringProject?: boolean;
+    recurringProjectPublishingDate?: Date|null;
+    mainImage: string;
+    images: string[];
+    organizationId: number;
+    open: string; 
+
     address: string;
-    lat?: number;
-    lng?: number;
     location: {
         lat: number;
         lng: number;
     };
     neededFunding: number;
-    raisedFunding: number;
-    description: string;
-    mainImage: string;
-    images: string[];
     organizationName: string;
-    organizationId: number;
-    open: string; 
+    
 }
