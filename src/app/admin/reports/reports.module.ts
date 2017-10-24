311fd843455498174010cd905313abec8a694b95
albannurkollari <32260816+albannurkollari@users.noreply.github.com>
2017-10-24 15:09:47 +0200

    Updated Reports Module to include SharedModule that already consists of Material modules needed for this modules components

--- src/app/admin/reports/reports.module.ts
+++ src/app/admin/reports/reports.module.ts
@@ -1,16 +1,15 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
+
+// Components
 import { ClosedProjectsComponent } from './closed-projects/closed-projects.component';
-import { MatTableModule, MatSortModule, MatCardModule, MatSelectModule, MatDatepickerModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatTableModule,
-    MatSortModule,
-    MatCardModule,
-    MatDatepickerModule,
-    MatSelectModule
+    SharedModule
   ],
   declarations: [ClosedProjectsComponent]
 })
